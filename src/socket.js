d1df4ef43b570350cb7ea33a765554a6b66ee39c
9oodam <godamcity98@gmail.com>
2024-12-09 17:29:42 +0900

    build

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://api.flrou.site", {
+export const socket = io("http://api.flrou.site", {
   withCredentials: true,
   transports: ["websocket"],
 });
